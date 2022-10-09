6eea7b1b21596d130271beafeca094bc3d3dcac9
Viktor Ishchenko <redbearddog@icloud.com>
2022-10-09 16:17:18 +0300

    constellation VIAL

---
+++ keyboards/aeboards/constellation/keymaps/vial-apple/config.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+/*  Apple FN  */
+#undef VENDOR_ID
+#define VENDOR_ID 0x05ac
+#undef PRODUCT_ID
+#define PRODUCT_ID 0x0267
+
+/* Unique keyboard ID for VIAL  */
+#define VIAL_KEYBOARD_UID {0x24, 0x2F, 0x8B, 0x71, 0x0C, 0x1F, 0x94, 0x14}
+
+/* Combination to Unlock: Esc (0,0) + Enter (2,12) */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+
+/* Mouse Speed */
+#define MOUSEKEY_MOVE_DELTA 2
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 10
+
+/* Toggle Layers on 2 taps */
+#define TAPPING_TOGGLE 2
---
+++ keyboards/aeboards/constellation/keymaps/vial-apple/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+NKRO_ENABLE = no            # Enable N-Key Rollover
+APPLE_FN_ENABLE = yes       # Enable Apple Fn key functionality
