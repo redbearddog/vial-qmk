d37dabe20d5a0ef75ae1051006cdcbaf7a16e5d0
Viktor Ishchenko <redbearddog@icloud.com>
2022-10-09 16:32:12 +0300

    zenith vial

---
+++ keyboards/wilba_tech/wt65_xt/keymaps/vial/config.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCB, 0x39, 0xAA, 0xBF, 0xC7, 0xE5, 0x80, 0xFE}
+
+/* Combination to Unlock: Esc (0,2) + Enter (2,14) */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 14 }
+
+/* Mouse Speed */
+#define MOUSEKEY_MOVE_DELTA 2
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 10
+
+/* Toggle Layers on 2 taps */
+#define TAPPING_TOGGLE 2
